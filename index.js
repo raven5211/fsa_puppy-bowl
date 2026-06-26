b1d9c8d4c072e3088875967a2083098cfb0d319f
Raven Bratcher <ravenbratcher@gmail.com>
2026-06-26 10:07:32 -0500

    added classes, api paths, and states

--- index.js
+++ index.js
@@ -1,13 +1,128 @@
-/*             Feel free to use this skeleton I have provided or delete everything and do your own thing!             */
+// === CLASSES AND TYPE DECLARATIONS ===
 
-//If you would like to, you can create a variable to store the API_URL here.
-//This is optional. if you do not want to, skip this and move on.
+/**
+ * class representing teams playing in the PuppyBowl
+ */
+class Team {
+  /**
+   * @param {number} id - The unique identifier for the team
+   * @param {string} name - The name of the team
+   * @param {number} score - The current score of the team
+   * @param {Player[]} players - A list of players on the team
+   */
+  constructor(id, name, score, players) {
+    /**
+     * The unique identifier for the team
+     * @type {number}
+     */
+    this.id = id;
+
+    /**
+     * The name of the team
+     * @type {string}
+     */
+    this.name = name;
+
+    /**
+     * The current score of the team
+     * @type {number}
+     */
+    this.score = score;
+
+    /**
+     * A list of players on the team
+     * @type {Player[]}
+     */
+    this.players = players;
+  }
+}
+
+/**
+ * class representing players playing in the PuppyBowl
+ */
+class Player {
+  /**
+   * @param {number} id - The unique identifier for the puppy
+   * @param {string} name - The name of the puppy
+   * @param {string} breed - The breed of the puppy
+   * @param {Status} status - The status of the puppy.
+   * @param {string} imageUrl - The URL to an image of the puppy
+   * @param {number | null} teamId - The ID of the team this puppy is on, or null if not on a team
+   */
+  constructor(id, name, breed, status, imageUrl, teamId) {
+    /**
+     * The unique identifier for the puppy
+     * @type {number}
+     */
+    this.id = id;
+
+    /**
+     * The name of the puppy
+     * @type {string}
+     */
+    this.name = name;
+
+    /**
+     * The breed of the puppy
+     * @type {string}
+     */
+    this.breed = breed;
+
+    /**
+     * The status of the puppy.
+     * @type {Status} - enum: {"bench", "field"}
+     *    - "bench": The puppy is currently on the bench. (default)
+     *    - "field": The puppy is currently playing in the field.
+     */
+    this.status = status;
+
+    /**
+     * The URL to an image of the puppy
+     * @type {string}
+     */
+    this.imageUrl = imageUrl;
+
+    /**
+     * The ID of the team this puppy is on, or null if not on a team
+     * @type {number | null}
+     */
+    this.teamId = teamId;
+  }
+}
+
+/**
+ * Enum for puppy status.
+ * @readonly
+ * @enum {string}
+ */
+const Status = {
+  /** The puppy is currently on the bench. (default) */
+  BENCH: "bench",
+  /** The puppy is currently playing in the field. */
+  FIELD: "field",
+};
+
+//////////////////////////////
+// === API INFO ===
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
+const COHORT = "/2605-RAVEN";
+
+const PLAYER_RESOURCE = "/players";
+const PLAYER_API = BASE + COHORT + PLAYER_RESOURCE;
+
+const TEAM_RESOURCE = "/teams";
+const TEAM_API = BASE + COHORT + TEAM_RESOURCE;
 
 /////////////////////////////
-/*This looks like a good place to declare any state or global variables you might need*/
+// === STATES ===
+/** @type {Player[]} */
+const players = [];
+
+/** @type {Team[]} */
+const teams = [];
 
 ////////////////////////////
-
+// === API FUNCTIONS ===
 /**
  * Fetches all players from the API.
  * This function should not be doing any rendering
@@ -64,6 +179,8 @@ const addNewPlayer = async (newPlayer) => {
 const removePlayer = async (playerId) => {
   //TODO
 };
+
+// === COMPONENT FUNCTIONS ===
 
 /**
  * Updates html to display a list of all players or a single player page.
